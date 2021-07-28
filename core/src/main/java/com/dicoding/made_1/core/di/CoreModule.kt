ec8575257a994361efcbdd9fe8a81fa1c45bd490
teguhmuflih <33649540+teguhmuflih@users.noreply.github.com>
2021-07-28 20:44:39 +0700

    add security

--- core/src/main/java/com/dicoding/made_1/core/di/CoreModule.kt
+++ core/src/main/java/com/dicoding/made_1/core/di/CoreModule.kt
@@ -1,3 +1,6 @@
+@file:Suppress("SpellCheckingInspection", "SpellCheckingInspection", "SpellCheckingInspection",
+    "SpellCheckingInspection", "SpellCheckingInspection"
+)
 package com.dicoding.made_1.core.di
 
 import androidx.room.Room
@@ -8,6 +11,9 @@ import com.dicoding.made_1.core.data.source.remote.RemoteDataSource
 import com.dicoding.made_1.core.data.source.remote.network.ApiService
 import com.dicoding.made_1.core.domain.repository.ISportRepository
 import com.dicoding.made_1.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -16,22 +22,32 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
+// Encryption
 val databaseModule = module {
     factory { get<SportDatabase>().sportDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             SportDatabase::class.java, "sport.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
-
+//certificate pinning
 val networkModule = module {
     single {
+        val hostname = "thesportsdb.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/ FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
